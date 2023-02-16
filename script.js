22d9e719aa914e9be680423a95b613ffcde5067d
MMundy <smmundyjr@crimson.ua.edu>
2023-02-16 17:02:51 -0600

    stuff

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function apiSearch() {
   $.ajax({
       url: 'https://api.bing.microsoft.com//v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
-          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "52016bcb31284612aee824446dc9bd4b");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "6c02ef8bc93044148fee838af4b553bb");
       },
       type: "GET",
     })
@@ -24,7 +24,9 @@ function apiSearch() {
       }
 
       $('#searchResults').html(results);
-      $('#searchResults').dialog();
+        $('#searchResults').dialog({
+            width: 500,
+            });
     })
     .fail(function () {
       alert("error");
@@ -36,39 +38,68 @@ $("#Search").click(function() {
 
 }); 
 
-$("#searchEngineName").click(function() {
+var backgroundImage = 0;
+$("#AskAthenaHeader").click(function() {
     /*Change the background image*/
-    function() {
-        if ($("body").css("background-color") == "lightgreen") { //Background image is the default image (green)
-            $("this").css("background-image", "url(https://unsplash.com/photos/pJadQetzTkI)"); //Sets background to blue gradient image
-            $("this").css("background-color", "lightgrey") //Couldn't figure out how to access the image url for comparison, so I just set a background color with each image to use for comparison
+        if (backgroundImage == 0) { //Background image is the default image (green)
+            backgroundImage++;
+            $("body").css("background-image", "url(https://images.unsplash.com/photo-1557683316-973673baf926?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=829&q=80)"); //Sets background to blue gradient image 
+            $("body").css("background-color", "lightgrey") //Couldn't figure out how to access the image url for comparison, so I just set a background color with each image to use for comparison
         }
-        else if ($("body").css("background-color") == "lightgrey") { //Background image is the blue gradient
-            $("this").css("background-image", "url(https://unsplash.com/photos/tgrBcf7S_dY)"); //Sets the background image to orange gradient
-            //$("this").css("background-color", "lightskyblue")
+        else if (backgroundImage == 1) { //Background image is the blue gradient
+            backgroundImage++;
+            $("body").css("background-image", "url(https://images.unsplash.com/photo-1557682260-96773eb01377?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=829&q=80)"); //Sets the background image to orange gradient
+            $("body").css("background-color", "lightskyblue")
         }
         else { //Background is blue gradient
-            $("body").css("background-image", "url(https://unsplash.com/photos/UWdTDa1Z9DM)")
-            $("this").css("background-color", "lightgreen")
+            backgroundImage = 0;
+            $("body").css("background-image", "url(https://images.unsplash.com/photo-1617957772097-93dc166cd335?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80)") //Sets background to the original (green gradient) image
+            $("body").css("background-color", "lightgreen")
         }
-    }
 });
 
-/*$("#currentTime").click(function() {
-    var date = new Date();
-    time = date.getHours() + ":" + date.getMinutes();
-    $("#time").innerHTML = time;
-    $("#time").dialog("open");
-})*/
-
-$(function() { //Gets the current time and loads it into the 'time' div, which is displayed as a jQuery dialog window
+$(function displayTime() { //Gets the current time and loads it into the 'time' div, which is displayed as a jQuery dialog window
     $("#time").dialog({
         autoOpen: false,
+
     });
     $("#currentTime").click(function () {
         var date = new Date();
-        time = date.getHours() + ":" + date.getMinutes();
-        $("#time").html = time;
+        var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+        hours = hours < 10 ? "0" + hours : hours;
+        var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes(); // time minutes < 10, minutes var = first digit of '0' + second digit of time minutes, else = time minutes value;
+        var amPM = date.getHours() >= 12 ? "PM" : "AM";
+        time = hours + ":" + minutes + " " + amPM;
+
+        $("#time").html(time);
         $("#time").dialog("open");
     });
 });
+
+$('#feelingLuckyButton').click(function () {
+    feelingLuckySearch();
+})
+
+function feelingLuckySearch() { //Modified version of the api search function
+    var params = {
+        "q": $("#query").val(),
+        "count": "50",
+        "offset": "0",
+        "mkt": "en-us"
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com//v7.0/search?' + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "6c02ef8bc93044148fee838af4b553bb");
+        },
+        type: "GET",
+    })
+        .done(function (data) {
+            len = data.webPages.value.length;
+            window.location.href = data.webPages.value[0].url; //simulates a mouse click and opens the set url. User maintains ability to click the back arrow to go back to the search engine page
+        })
+        .fail(function () {
+            alert("error");
+        });
+}
