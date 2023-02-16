d041d2a25fa16fdebd4ae9ca8e2fc107aecba614
MMundy <smmundyjr@crimson.ua.edu>
2023-02-15 21:25:15 -0600

    project files

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var len;
 var results = '';
+var time;
 
 function apiSearch() {
   var params = {
@@ -10,9 +11,9 @@ function apiSearch() {
   };
 
   $.ajax({
-      url: 'my-api-url' + $.param(params),
+      url: 'https://api.bing.microsoft.com//v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "52016bcb31284612aee824446dc9bd4b");
       },
       type: "GET",
     })
@@ -29,3 +30,45 @@ function apiSearch() {
       alert("error");
     });
 }
+
+$("#Search").click(function() {
+    apiSearch();
+
+}); 
+
+$("#searchEngineName").click(function() {
+    /*Change the background image*/
+    function() {
+        if ($("body").css("background-color") == "lightgreen") { //Background image is the default image (green)
+            $("this").css("background-image", "url(https://unsplash.com/photos/pJadQetzTkI)"); //Sets background to blue gradient image
+            $("this").css("background-color", "lightgrey") //Couldn't figure out how to access the image url for comparison, so I just set a background color with each image to use for comparison
+        }
+        else if ($("body").css("background-color") == "lightgrey") { //Background image is the blue gradient
+            $("this").css("background-image", "url(https://unsplash.com/photos/tgrBcf7S_dY)"); //Sets the background image to orange gradient
+            //$("this").css("background-color", "lightskyblue")
+        }
+        else { //Background is blue gradient
+            $("body").css("background-image", "url(https://unsplash.com/photos/UWdTDa1Z9DM)")
+            $("this").css("background-color", "lightgreen")
+        }
+    }
+});
+
+/*$("#currentTime").click(function() {
+    var date = new Date();
+    time = date.getHours() + ":" + date.getMinutes();
+    $("#time").innerHTML = time;
+    $("#time").dialog("open");
+})*/
+
+$(function() { //Gets the current time and loads it into the 'time' div, which is displayed as a jQuery dialog window
+    $("#time").dialog({
+        autoOpen: false,
+    });
+    $("#currentTime").click(function () {
+        var date = new Date();
+        time = date.getHours() + ":" + date.getMinutes();
+        $("#time").html = time;
+        $("#time").dialog("open");
+    });
+});
